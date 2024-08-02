4c6754125b4159f96ed01e1aaa97722bc509ae05
Jeb <130810521+jebarpg@users.noreply.github.com>
2024-08-01 22:08:20 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-hub checkout ${{ github.server_url }}/${{ github.repository }}
+hub checkout ${GIT_REPO}
 VERSION_NAME=`grep -oP 'versionName "\K(.*?)(?=")' ./${APP_FOLDER}/build.gradle`
 hub release create -a ./${APP_FOLDER}/build/outputs/apk/release/**.apk -m "v${GITHUB_REF##*/}" ${GITHUB_REF##*/} 
