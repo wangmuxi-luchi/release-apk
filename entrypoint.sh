0ef14f5f3b386ae4818ee27e53e657469ec5fc63
Jeb <130810521+jebarpg@users.noreply.github.com>
2024-08-02 19:38:51 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,9 @@
 hub checkout ${GIT_REPO}
 git config --global --add safe.directory /github/workspace
 VERSION_NUMBER=`grep -oP '"version": "\K(.*?)(?=")' ./package.json`
-hub release edit -a ./${APP_FOLDER}/build/outputs/apk/release/**.apk -m "" v${VERSION_NUMBER}
-hub release edit -a ./${APP_FOLDER}/build/outputs/bundle/release/**.aab -m "" v${VERSION_NUMBER}
+if [ -f ./${APP_FOLDER}/build/outputs/apk/release/**.apk ]; then
+  hub release edit -a ./${APP_FOLDER}/build/outputs/apk/release/**.apk -m "" v${VERSION_NUMBER}
+fi
+if [ -f ./${APP_FOLDER}/build/outputs/bundle/release/**.aab ]; then
+  hub release edit -a ./${APP_FOLDER}/build/outputs/bundle/release/**.aab -m "" v${VERSION_NUMBER}
+fi
