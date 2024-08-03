fc4eb11cb85b2c8679d21b2a28ac461510c44d1b
Jeb <130810521+jebarpg@users.noreply.github.com>
2024-08-02 22:48:12 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,16 +3,20 @@
 hub checkout ${GIT_REPO}
 git config --global --add safe.directory /github/workspace
 VERSION_NUMBER=`grep -oP '"version": "\K(.*?)(?=")' ./package.json`
-RPOJECT_NAME=`grep -oP '"name": "\K(.*?)(?=")' ./package.json`
+PROJECT_NAME=`grep -oP '"name": "\K(.*?)(?=")' ./package.json`
 if [ -f ./${APP_FOLDER}/build/outputs/apk/release/**.apk ]; then
     for f in ./${APP_FOLDER}/build/outputs/apk/release/**.apk; do
-        rename 's/app-/${RPOJECT_NAME}_${VERSION_NUMBER}_/' $f
+        STRING=${PROJECT_NAME}_${VERSION_NUMBER}_
+        echo $ENV{$STRING}
+        rename 's/app-/\Q$ENV{$STRING}\E/' $f
     done
   hub release edit -a ./${APP_FOLDER}/build/outputs/apk/release/**.apk -m "" v${VERSION_NUMBER}
 fi
 if [ -f ./${APP_FOLDER}/build/outputs/bundle/release/**.aab ]; then
 for f in ./${APP_FOLDER}/build/outputs/apk/release/**.aab; do
-        rename 's/app-/${RPOJECT_NAME}_${VERSION_NUMBER}_/' $f
+        STRING=${PROJECT_NAME}_${VERSION_NUMBER}_
+        echo $ENV{$STRING}
+        rename 's/app-/\Q$ENV{$STRING}\E/' $f
     done
   hub release edit -a ./${APP_FOLDER}/build/outputs/bundle/release/**.aab -m "" v${VERSION_NUMBER}
 fi
