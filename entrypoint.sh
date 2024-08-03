1b2f580275da813cb9f972cb92b89fd3275e24a9
Jeb <130810521+jebarpg@users.noreply.github.com>
2024-08-02 21:51:41 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,16 @@
 hub checkout ${GIT_REPO}
 git config --global --add safe.directory /github/workspace
 VERSION_NUMBER=`grep -oP '"version": "\K(.*?)(?=")' ./package.json`
+RPOJECT_NAME=`grep -oP '"name": "\K(.*?)(?=")' ./package.json`
 if [ -f ./${APP_FOLDER}/build/outputs/apk/release/**.apk ]; then
+    for f in ./${APP_FOLDER}/build/outputs/apk/release/**.apk; do
+        rename 's/app-/${RPOJECT_NAME}_${VERSION_NUMBER}_/' $f
+    done
   hub release edit -a ./${APP_FOLDER}/build/outputs/apk/release/**.apk -m "" v${VERSION_NUMBER}
 fi
 if [ -f ./${APP_FOLDER}/build/outputs/bundle/release/**.aab ]; then
+for f in ./${APP_FOLDER}/build/outputs/apk/release/**.aab; do
+        rename 's/app-/${RPOJECT_NAME}_${VERSION_NUMBER}_/' $f
+    done
   hub release edit -a ./${APP_FOLDER}/build/outputs/bundle/release/**.aab -m "" v${VERSION_NUMBER}
 fi
