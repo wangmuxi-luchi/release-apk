ee822c17b099a5aaac308205a01f1a29647b13e1
wangmuxi-luchi <18593825310@163.com>
2025-03-18 20:08:23 +0800

    add echo to debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,16 @@
 
 hub checkout ${GIT_REPO}
 git config --global --add safe.directory /github/workspace
-VERSION_NUMBER=`grep -oP '"version": "\K(.*?)(?=")' ./package.json`
-PROJECT_NAME=`grep -oP '"name": "\K(.*?)(?=")' ./package.json`
+VERSION_NUMBER=$(grep -oP 'versionName "\K(.*?)(?=")' app/build.gradle)
+PROJECT_NAME=$(grep -oP 'applicationId "\K(.*?)(?=")' app/build.gradle)
+
+# VERSION_NUMBER=`grep -oP '"version": "\K(.*?)(?=")' ./package.json`
+# PROJECT_NAME=`grep -oP '"name": "\K(.*?)(?=")' ./package.json`
 APK_FILES=(./${APP_FOLDER}/build/outputs/apk/release/**.apk)
+for file in ./${APP_FOLDER}/build/outputs/apk/release/*; do
+    echo files before rename
+    echo "$file"
+done
 if [ -f "${APK_FILES[0]}" ]; then
     # Rename the 'app-' part with ${PROJECT_NAME}_${VERSION_NUMBER}_
     for f in "${APK_FILES[@]}"; do
@@ -16,6 +23,12 @@ if [ -f "${APK_FILES[0]}" ]; then
     for f in "${CHANGED_APK_FILES[@]}"; do
         rename 's/-/_/' "$f"
     done
+
+    for file in ./${APP_FOLDER}/build/outputs/apk/release/*; do
+        echo files after rename
+        echo "$file"
+    done
+
     if hub release edit -a ./${APP_FOLDER}/build/outputs/apk/release/**_signed.apk -m "" v${VERSION_NUMBER}; then
         echo added APK release
     else
